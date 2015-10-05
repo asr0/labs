76e7b920a173130d593c58a963a9799696b390a8
Anderson Menezes <andersomxp@gmail.com>
2015-10-05 09:44:56 -0300

    property string.length

---
+++ javascript/string/property/length.js
@@ -0,0 +1,13 @@
+/*
+	Retorna a quantidade de itens(letra ou números) que existe na string.
+	PS: Essa propriedade também é usado em Arrays.
+*/
+var myName = 'Anderson Menezes';
+console.log( myName.length ); // 16
+
+/*
+	Se a string tiver um valor boleano, ele sempre retornará undefined.
+*/
+
+var casado = false;
+console.log( casado.length ); // undefined
