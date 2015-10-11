96419919865923370d8fdf23993eed6870703cab
Anderson Menezes <andersomxp@gmail.com>
2015-10-11 00:11:45 -0300

    Array length

---
+++ javascript/array/properties/length.js
@@ -0,0 +1,9 @@
+var arrTeste = [ 'HTML5', 'CSS3', 'Ruby', 'Python', 'PHP', 'Javascript' ];
+
+console.log( arrTeste.length ); //6
+
+for ( var i = 0; i <= 20; i++ ){
+	arr.push(i);
+}
+
+console.log( arr.length ); // 21
