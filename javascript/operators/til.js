1dbb226905e23b55b539b6639b0527b3b378aa74
Anderson Menezes <andersomxp@gmail.com>
2015-11-16 08:36:48 -0300

    Operador til

---
+++ javascript/operators/til.js
@@ -0,0 +1,12 @@
+// Verificando o primeiro item do array
+var humans = ['John', 'Hance', 'Buch'];
+
+if( ~~humans.indexOf() )
+	console.log(humans[~~humans]);
+
+var number = 23.52626284;
+console.log( ~number );
+
+// Números são decimais são convertidos em números inteiros.
+var number = 23.52626284;
+console.log( ~~number );
